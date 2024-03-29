84d3a476cb4152cad072e531e9585a03d15a2c44
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-03-29 09:48:09 +0530

    Mastering Design Principles - SOLID (Module-I & II)

--- 4. SOLID Principles/Example.java
+++ 4. SOLID Principles/Example.java
@@ -94,7 +94,7 @@ public class Example {
 */
 
 
-// Not Violet LSP
+/* Not Violet LSP
 // Supar Class: Bird
 class Bird{
     public void fun(Bird bird){
@@ -185,3 +185,302 @@ public class Example{
         penguin.walk(); // Penguin is walking
     }
 }
+*/
+
+
+/* Violet LSP
+// Supar Class: Vehicle
+class Vehicle{
+    public void startEngine(){
+        System.out.println("Vehicle may have engine");
+    }
+    public void doMovement(){
+        System.out.println("Vehicle can move");
+    }
+    public void fly(){
+        System.out.println("Vehicle may fly");
+    }
+}
+
+// Subclass: Car
+class Car extends Vehicle { 
+    @Override
+    public void startEngine(){
+        System.out.println("Car have engine");
+    }
+    @Override
+    public void doMovement() {
+        System.out.println("Car can move");
+    }
+    // Exception 1: Car cannot fly, so we don't override the fly method
+}
+
+// Subclass: Cycle
+class Cycle extends Vehicle { 
+    // Exception 1: Cycle does not have engine, so we don't override the startEngine method
+    @Override
+    public void doMovement() {
+        System.out.println("Cycle can move");
+    }
+    // Exception 2: Cycle cannot fly, so we don't override the fly method
+}
+
+// Subclass: Airplane
+class Airplane extends Vehicle { 
+    @Override
+    public void startEngine() {
+        System.out.println("Airplane have engine");
+    }
+    @Override
+    public void doMovement() {
+        System.out.println("Airplane can move");
+    }
+    @Override
+    public void fly() {
+        System.out.println("Airplane can fly");
+    }
+}
+
+public class Example{
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle cycle = new Cycle();
+        Vehicle airplane = new Airplane();
+
+        // Car violet LSP
+        car.startEngine(); // Car have engine
+        car.doMovement(); // Car can move
+        car.fly(); // Vehicle may fly
+
+        // Cycle violet LSP
+        cycle.startEngine(); // Vehicle may have engine
+        cycle.doMovement(); // Cycle can move
+        cycle.fly(); // Vehicle may fly
+
+        // Only Airplane not violet LSP
+        airplane.startEngine(); // Airplane have engine
+        airplane.doMovement(); // Airplane can move
+        airplane.fly(); // Airplane can fly
+    }
+}
+
+*/
+
+
+/* Not Violet LSP
+
+// Supar Class: Vehicle
+class Vehicle{
+    // Consumer Method
+    public void consumerFun(Vehicle vehicle){
+        vehicle.doMovement();
+    }
+    public void doMovement(){
+        System.out.println("Vehicle can move");
+    }
+}
+
+
+// Interface: IHaveEngine
+interface IHaveEngine {
+    void startEngine();
+}
+
+// Interface: IFlyable
+interface IFlyable{
+    void fly();
+}
+
+// Subclass: Car
+class Car extends Vehicle implements IHaveEngine{ 
+    @Override
+    public void startEngine(){
+        System.out.println("Car have engine");
+    }
+    @Override
+    public void doMovement() {
+        System.out.println("Car can move");
+    }
+}
+
+// Subclass: Cycle
+class Cycle extends Vehicle { 
+    @Override
+    public void doMovement() {
+        System.out.println("Cycle can move");
+    }
+}
+
+// Multiple Inheritance for Airplane
+// interface IFlyable_IHaveEngine extends IHaveEngine, IFlyable{}
+
+// Subclass: Airplane
+class Airplane extends Vehicle implements IHaveEngine, IFlyable { 
+    @Override
+    public void startEngine() {
+        System.out.println("Airplane have engine");
+    }
+    @Override
+    public void doMovement() {
+        System.out.println("Airplane can move");
+    }
+    @Override
+    public void fly() {
+        System.out.println("Airplane can fly");
+    }
+}
+
+public class Example{
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Vehicle car = new Car();
+        Vehicle cycle = new Cycle();
+        Vehicle airplane = new Airplane();
+
+        // Meaning Of Substitution: Any derived class should be able to substitute its parent class without the consumer knowing it
+        vehicle.consumerFun(vehicle); // Vehicle can move
+        vehicle.consumerFun(car); // Car can move
+        vehicle.consumerFun(cycle); // Cycle can move
+        vehicle.consumerFun(airplane); // Airplane can move
+    }
+}
+*/
+
+/* Violet LSP
+// Supar Class: Human
+class Human{
+    public void eat(){
+        System.out.println("Human is eating");
+    }
+    public void sleep(){
+        System.out.println("Human is sleeping");
+    }
+    public void work(){
+        System.out.println("Human can work");
+    }
+    public void makeDinner(){
+        System.out.println("Human can make dinner");
+    }
+}
+
+// Subclass: Child
+class Child extends Human { 
+    public void eat(){
+        System.out.println("Child is eating");
+    }
+    public void sleep(){
+        System.out.println("Child is sleeping");
+    }
+    // Exception 1: Child cannot work, so we don't override the work method
+    // Exception 2: Child cannot make dinner, so we don't override the makeDinner method
+}
+
+// Subclass: Adult
+class Adult extends Human { 
+    public void eat(){
+        System.out.println("Adult is eating");
+    }
+    public void sleep(){
+        System.out.println("Adult is sleeping");
+    }
+    public void work(){
+        System.out.println("Adult can work");
+    }
+    public void makeDinner(){
+        System.out.println("Adult can make dinner");
+    }
+}
+
+public class Example{
+    public static void main(String[] args) {
+        Human child = new Child();
+        Human adult = new Adult();
+
+        // Child violet LSP
+        child.eat(); // Child is eating
+        child.sleep(); // Child is sleeping
+        child.work(); // Human can work
+        child.makeDinner(); // Human can make dinner
+    
+
+        // Only adult not violet LSP
+        adult.eat(); // Adult is eating
+        adult.sleep(); // Adult is sleeping
+        adult.work(); // Adult can work
+        adult.makeDinner(); // Adult can make dinner        
+    }
+}
+*/
+
+// Not Violet LSP
+// Supar Class: Human
+class Human{
+    public void consumerFun(Human human){
+        human.eat();
+        human.sleep();
+    }
+    public void eat(){
+        System.out.println("Human is eating");
+    }
+    public void sleep(){
+        System.out.println("Human is sleeping");
+    }
+}
+
+// Interface: IDoWork
+interface IDoWork{
+    void work();
+}
+
+// Interface: IMakeDinner
+interface IMakeDinner{
+    void makeDinner();
+}
+
+
+// Subclass: Child
+class Child extends Human { 
+    public void eat(){
+        System.out.println("Child is eating");
+    }
+    public void sleep(){
+        System.out.println("Child is sleeping");
+    }
+}
+
+// Subclass: Adult
+class Adult extends Human implements IDoWork, IMakeDinner { 
+    public void eat(){
+        System.out.println("Adult is eating");
+    }
+    public void sleep(){
+        System.out.println("Adult is sleeping");
+    }
+    public void work(){
+        System.out.println("Adult can work");
+    }
+    public void makeDinner(){
+        System.out.println("Adult can make dinner");
+    }
+}
+
+public class Example{
+    public static void main(String[] args) {
+        Human human = new Human();
+        Human child = new Child();
+        Human adult = new Adult();
+
+        human.consumerFun(human);     
+        human.consumerFun(child); 
+        human.consumerFun(adult);
+    }
+}
+
+/*
+Expected Output:
+Human is sleeping
+Child is eating
+Child is sleeping
+Adult is eating
+Adult is sleeping
+*/
