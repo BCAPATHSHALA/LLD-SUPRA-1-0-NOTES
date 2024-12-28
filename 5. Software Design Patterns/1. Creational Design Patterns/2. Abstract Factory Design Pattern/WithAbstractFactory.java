8b93146493166651edf1837da145e2a06a13feb2
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-12-28 14:51:42 +0530

    Add 2. Abstract Factory Design Pattern

--- 5. Software Design Patterns/1. Creational Design Patterns/2. Abstract Factory Design Pattern/WithAbstractFactory.java
+++ 5. Software Design Patterns/1. Creational Design Patterns/2. Abstract Factory Design Pattern/WithAbstractFactory.java
@@ -9,24 +9,28 @@ interface TextField {
 
 // Step 2: Concrete Products
 class LightButton implements Button {
+    @Override
     public void display() {
         System.out.println("Light Button");
     }
 }
 
 class DarkButton implements Button {
+    @Override
     public void display() {
         System.out.println("Dark Button");
     }
 }
 
 class LightTextField implements TextField {
+    @Override
     public void display() {
         System.out.println("Light TextField");
     }
 }
 
 class DarkTextField implements TextField {
+    @Override
     public void display() {
         System.out.println("Dark TextField");
     }
@@ -40,20 +44,24 @@ interface ThemeFactory {
 
 // Step 4: Concrete Factories
 class LightThemeFactory implements ThemeFactory {
+    @Override
     public Button createButton() {
         return new LightButton();
     }
 
+    @Override
     public TextField createTextField() {
         return new LightTextField();
     }
 }
 
 class DarkThemeFactory implements ThemeFactory {
+    @Override
     public Button createButton() {
         return new DarkButton();
     }
 
+    @Override
     public TextField createTextField() {
         return new DarkTextField();
     }
